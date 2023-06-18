c9f9604cd39553f562775c5c8cffb7f59d2ddc92
Kathiravan K <kathirr007@gmail.com>
2023-06-19 00:36:10 +0530

    Added script to check if dist folder exists or not before push

---
+++ prepush.mjs
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+import { exec } from 'node:child_process';
+
+// Define the command you want to execute
+const npmScript = 'npm run build';
+
+// Define the path to the folder
+const folderPath = './dist';
+const filePath = './dist/index.html';
+
+// Check if the folder exists
+fs.access(filePath, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.error('Index.html file does not exist. Running build command...');
+    // Execute the npm script
+    exec(npmScript, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing npm script: ${error}`);
+        return;
+      }
+
+      // Log the output of the script
+      console.log(`stdout: ${stdout}`);
+      // console.error(`stderr: ${stderr}`);
+    });
+    return;
+  }
+
+  console.log('Folder exists');
+});
