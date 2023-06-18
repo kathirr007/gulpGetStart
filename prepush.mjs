69002c10cccc3ad2aa93a67eefdd98b54edb1e7a
Kathiravan K <kathirr007@gmail.com>
2023-06-19 00:55:56 +0530

    Fresh dist build created & pushed

--- prepush.mjs
+++ prepush.mjs
@@ -4,7 +4,7 @@ import { exec } from 'node:child_process';
 // Define the command you want to execute
 const npmScript = 'npm run build';
 const prePushScript = 'npm run prepush';
-const pushScript = 'git add . && git push';
+const pushScript = 'git add . && git commit -m "Fresh dist build created & pushed" && git push';
 
 // Define the path to the folder
 const folderPath = './dist';
@@ -37,8 +37,8 @@ fs.access(filePath, fs.constants.F_OK, (err) => {
       // Log the output of the script
       console.log(`stdout: ${stdout}`);
       // console.error(`stderr: ${stderr}`);
+      return;
     });
-    return;
   }
 
 });
